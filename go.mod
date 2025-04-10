3b5016366ecb76b41dbb620e00483674cdcfc3bc
zhaoxiaojie <zhaoxiaojie@ailinking.com>
2025-04-10 10:57:38 +0000

    add lucence query support

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ toolchain go1.23.8
 
 require (
 	github.com/apache/iotdb-client-go v1.3.3
-	github.com/linuxsuren/api-testing v0.0.19
+	github.com/linuxsuren/api-testing v0.0.20-0.20250331081733-9d3e7ad6d47b
 	github.com/spf13/cobra v1.9.1
 	google.golang.org/protobuf v1.36.6
 )
@@ -21,6 +21,9 @@ require (
 	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/bufbuild/protocompile v0.6.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cucumber/gherkin-go/v19 v19.0.3 // indirect
+	github.com/cucumber/godog v0.12.6 // indirect
+	github.com/cucumber/messages-go/v16 v16.0.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/evanphx/json-patch v0.5.2 // indirect
 	github.com/expr-lang/expr v1.15.6 // indirect
@@ -31,15 +34,22 @@ require (
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
 	github.com/go-openapi/spec v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
+	github.com/gofrs/uuid v4.2.0+incompatible // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
+	github.com/hashicorp/go-memdb v1.3.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/huandu/xstrings v1.4.0 // indirect
+	github.com/iancoleman/orderedmap v0.3.0 // indirect
 	github.com/imdario/mergo v0.3.16 // indirect
+	github.com/invopop/jsonschema v0.7.0 // indirect
 	github.com/jhump/protoreflect v1.15.3 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/linuxsuren/go-fake-runtime v0.0.4 // indirect
+	github.com/linuxsuren/go-service v0.0.0-20231225060426-efabcd3a5161 // indirect
 	github.com/linuxsuren/oauth-hub v0.0.0-20240809060240-e78c21b5d8d4 // indirect
 	github.com/linuxsuren/unstructured v0.0.1 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
--- pkg/data_query.go
+++ pkg/data_query.go
@@ -16,10 +16,13 @@ limitations under the License.
 package pkg
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
+	"slices"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/elastic/go-elasticsearch/v8"
@@ -36,8 +39,17 @@ func (s *dbserver) Query(ctx context.Context, query *server.DataQuery) (result *
 	result = &server.DataQueryResult{
 		Data:  []*server.Pair{},
 		Items: make([]*server.Pairs, 0),
-		Meta:  &server.DataMeta{},
+		Meta: &server.DataMeta{
+			CurrentDatabase: query.Key,
+		},
 	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		esQuery(ctx, db, []string{query.Key}, query.Sql, result)
+	}()
 
 	// query data
 	if query.Sql == "" {
@@ -46,34 +58,101 @@ func (s *dbserver) Query(ctx context.Context, query *server.DataQuery) (result *
 
 	var dataResult *server.DataQueryResult
 	now := time.Now()
-	if dataResult, err = sqlQuery(ctx, []string{query.Sql}, db); err == nil {
+	if dataResult, err = sqlQuery(ctx, []string{query.Key}, query.Sql, db); err == nil {
 		result.Items = dataResult.Items
 		result.Meta.Duration = time.Since(now).String()
+	}
+	wg.Wait()
+	return
+}
+
+func esQuery(ctx context.Context, db *elasticsearch.Client, index []string, sql string, dataResult *server.DataQueryResult) (result *server.DataQueryResult, err error) {
+	resp, err := db.Indices.Get([]string{"*"})
+	if !resp.IsError() {
+		var r map[string]interface{}
+		if err = json.NewDecoder(resp.Body).Decode(&r); err == nil {
+			for k := range r {
+				dataResult.Meta.Databases = append(dataResult.Meta.Databases, k)
+			}
+
+			slices.Sort(dataResult.Meta.Databases)
+		}
+	}
+
+	if resp, err = db.Count(createCountRequests(ctx, db, index, sql)...); err == nil {
+		var body []byte
+		body, err = io.ReadAll(resp.Body)
+		if err != nil {
+			return
+		}
+
+		var response CountResponse
+		err = json.Unmarshal(body, &response)
+		if err != nil {
+			return
+		}
+		dataResult.Meta.Labels = append(dataResult.Meta.Labels, &server.Pair{
+			Key:   "total",
+			Value: fmt.Sprintf("%d", response.Count),
+		})
 	}
 	return
 }
 
-func sqlQuery(ctx context.Context, index []string, db *elasticsearch.Client) (result *server.DataQueryResult, err error) {
+type CountResponse struct {
+	Count int `json:"count"`
+}
+
+func createCountRequests(ctx context.Context, db *elasticsearch.Client, index []string, sql string) []func(*esapi.CountRequest) {
+	searchRequests := []func(*esapi.CountRequest){
+		db.Count.WithContext(ctx),
+		db.Count.WithIndex(index...),
+		db.Count.WithPretty(),
+	}
+	if sql == "" {
+		searchRequests = append(searchRequests, db.Count.WithBody(strings.NewReader(`{
+			"query": {
+				"match_all": {}
+			}
+		}`)))
+	} else {
+		searchRequests = append(searchRequests, db.Count.WithQuery(sql))
+	}
+	return searchRequests
+}
+
+func createSearchRequests(ctx context.Context, db *elasticsearch.Client, index []string, sql string) []func(*esapi.SearchRequest) {
+	searchRequests := []func(*esapi.SearchRequest){
+		db.Search.WithContext(ctx),
+		db.Search.WithSize(100),
+		db.Search.WithTrackTotalHits(true),
+		db.Search.WithIndex(index...),
+		db.Search.WithPretty(),
+	}
+	if sql == "" {
+		searchRequests = append(searchRequests, db.Search.WithBody(strings.NewReader(`{
+			"query": {
+				"match_all": {}
+			}
+		}`)))
+	} else {
+		searchRequests = append(searchRequests, db.Search.WithQuery(sql))
+	}
+	return searchRequests
+}
+
+func sqlQuery(ctx context.Context, index []string, sql string, db *elasticsearch.Client) (result *server.DataQueryResult, err error) {
 	result = &server.DataQueryResult{
 		Data:  []*server.Pair{},
 		Items: make([]*server.Pairs, 0),
 		Meta:  &server.DataMeta{},
 	}
 
-	var buf bytes.Buffer
-	query := map[string]interface{}{}
-	if err = json.NewEncoder(&buf).Encode(query); err != nil {
-		return
-	}
+	fmt.Printf("query from index [%v], sql [%s]\n", index, sql)
+	searchRequests := createSearchRequests(ctx, db, index, sql)
 
 	var res *esapi.Response
-	if res, err = db.Search(
-		db.Search.WithContext(ctx),
-		db.Search.WithIndex(index...),
-		db.Search.WithBody(&buf),
-		db.Search.WithTrackTotalHits(true),
-		db.Search.WithPretty(),
-	); err != nil {
+	if res, err = db.Search(searchRequests...); err != nil {
 		return
 	}
 
